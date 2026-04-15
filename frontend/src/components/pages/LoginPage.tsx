8a941f41adc6de0f2afe79d02f90f50d94ce18af
RjGutierrezz <rbg002@uark.edu>
2026-04-14 20:06:17 -0700

    finished left panel of login

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,7 +1,25 @@
 const LoginPage = () => {
 	return (
-		<div>
-			<p>Logging in</p>
+		<div className="login-page" style={{ position: "relative", zIndex: 1 }}>
+			<div className="left-login-panel">
+				<h1>Aura Drip</h1>
+
+        <span className="login-main-text">Your Wardrobe,</span>
+        <h3 className="login-sub-text">curated by us</h3>
+
+        <p className="login-text">Your personal wardrobe space where you can organize your pieces,
+        discover new outfit ideas, and plan what to wear with confidence every
+        single day. </p>
+
+        <div className="prompt-tip-chips">
+          <span>Outfit generation from your own clothes</span>
+          <span>Weather-aware styling suggestions</span>
+          <span>Organize by category, color, and vibe</span>
+        </div>
+			</div>
+			<div className="right-login-panel">
+        <h2>Signup</h2>
+      </div>
 		</div>
 	);
 };
